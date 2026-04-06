831dc90127254dc0ecda68adcad3061af88ff2ca
ShiQi-17 <19136769323@163.com>
2026-04-06 17:45:36 +0800

    更新 PostDetailActivity.kt

--- app/src/main/java/com/example/toolbox/community/PostDetailActivity.kt
+++ app/src/main/java/com/example/toolbox/community/PostDetailActivity.kt
@@ -1048,7 +1048,7 @@ fun ContentRenderer(
     when (content.type) {
         "image" -> {}
         "post" -> {
-            Text(content.title, style = MaterialTheme.typography.bodyLarge)
+            Text(content.title, fontWeight = FontWeight.Bold, style = MaterialTheme.typography.titleMedium)
             if (msg.is_markdown) MarkdownText(content.content ?: "") else Text(
                 content.content ?: "", style = MaterialTheme.typography.bodyLarge
             )
